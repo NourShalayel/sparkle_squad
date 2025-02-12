de9f5ca8cea1e3e8a9ff429d53278e26b1356577
LamaHafiz <lamahafiz2004@gmail.com>
2025-02-12 16:25:35 +0200

    Managing Data

--- src/components/department/department.component.tsx
+++ src/components/department/department.component.tsx
@@ -1,14 +1,5 @@
+import { departments } from "../../data/data";
 import "./Department.css";
-import { FaHeart, FaBell, FaChild, FaBrain, FaSnowflake, FaHandsHelping } from "react-icons/fa";
-
-const departments = [
-  { id: 1, name: "Emergency Department", icon: <FaBell /> },
-  { id: 2, name: "Cardiology Department", icon: <FaHeart /> },
-  { id: 3, name: "Therapy Department", icon: <FaHandsHelping /> },
-  { id: 4, name: "Pediatric Department", icon: <FaChild /> },
-  { id: 5, name: "Psychiatry Department", icon: <FaBrain /> },
-  { id: 6, name: "Psychiatry Department", icon: <FaSnowflake /> }
-];
 
 const Department = () => {
   return (
--- src/components/values-page/values-page.component.tsx
+++ src/components/values-page/values-page.component.tsx
@@ -1,34 +1,5 @@
+import { valuesData } from '../../data/data';
 import './values.css';
-import trustImage from '../../assets/Trust.png';
-import respectImage from '../../assets/Onboarding.png';
-import handImage from '../../assets/Handshake.png';
-
-const valuesData = [
-  {
-    id: 1,
-    image: trustImage,
-    title: "Compassion",
-    description: "We believe in treating every patient with kindness and empathy, listening to their concerns, and supporting them through each step of their journey."
-  },
-  {
-    id: 2,
-    image: handImage,
-    title: "Integrity",
-    description: "Honesty, transparency, and accountability are at the core of our actions, creating an environment of trust where patients and families can have confidence in our care."
-  },
-  {
-    id: 3,
-    image: respectImage,
-    title: "Respect",
-    description: "Respect is at the heart of our interactions, fostering an inclusive, supportive environment where everyone is treated with courtesy and understanding."
-  },
-  {
-    id: 4,
-    image: respectImage,
-    title: "Teamwork",
-    description: "We are committed to providing you with the best medical and healthcare services to help you live healthier and happier."
-  }
-];
 
 const Values = () => {
   return (
